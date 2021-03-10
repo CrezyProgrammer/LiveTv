c851b9447891ae4664bd09e0b77436e553b4b3d5
universalcopy <amikhokon441@gmail.com>
2021-03-10 21:20:18 +0600

    Initial commit

---
+++ app/src/main/java/com/live/tv/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.live.tv
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.extractor.ExtractorsFactory
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
+import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    // creating a variable for exoplayer
+    var exoPlayer: SimpleExoPlayer? = null
+
+    // url of video which we are loading.
+    var videoURL =
+        ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main)
+videoURL= intent.getStringExtra("link").toString()
+
+
+            // bandwisthmeter is used for
+            // getting default bandwidth
+            val bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter()
+
+            // track selector is used to navigate between
+            // video using a default seekbar.
+            val trackSelector: TrackSelector =
+                DefaultTrackSelector(AdaptiveTrackSelection.Factory(bandwidthMeter))
+
+            // we are adding our track selector to exoplayer.
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+
+            // we are parsing a video url
+            // and parsing its video uri.
+            val videouri = Uri.parse(videoURL)
+
+            // we are creating a variable for datasource factory
+            // and setting its user agent as 'exoplayer_view'
+            val dataSourceFactory = DefaultHttpDataSourceFactory("exoplayer_video")
+
+            // we are creating a variable for extractor factory
+            // and setting it to default extractor factory.
+            val extractorsFactory: ExtractorsFactory = DefaultExtractorsFactory()
+
+            // we are creating a media source with above variables
+            // and passing our event handler as null,
+            val mediaSource: MediaSource =
+               HlsMediaSource.Factory(DefaultHttpDataSourceFactory("userAgent"))
+                    .createMediaSource(videouri)
+            // inside our exoplayer view
+            // we are setting our player
+            exoPlayerView!!.player = exoPlayer
+
+            // we are preparing our exoplayer
+            // with media source.
+            exoPlayer!!.prepare(mediaSource)
+
+            // we are setting our exoplayer
+            // when it is ready.
+            exoPlayer!!.playWhenReady = true
+        exoPlayer!!.addListener(object : Player.EventListener {
+            override fun onTracksChanged(
+                trackGroups: TrackGroupArray,
+                trackSelections: TrackSelectionArray
+            ) {
+                Log.d("SAMPLE", "TrackGroupArray size=${trackGroups?.length}")
+                Log.d("SAMPLE", "TrackSelectionArray size=${trackSelections?.length}")
+            }
+
+            override fun onPlayerError(error: ExoPlaybackException) {
+                error?.let {
+                    Log.e("123321", it.message, it)
+                    exoPlayer?.retry()
+                }
+            }
+
+            override fun onLoadingChanged(isLoading: Boolean) {
+                Log.d("SAMPLE", "Loading = $isLoading")
+            }
+
+            override fun onPositionDiscontinuity(reason: Int) {
+                // noop
+            }
+
+            override fun onRepeatModeChanged(repeatMode: Int) {
+                // noop
+            }
+
+            override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
+                // noop
+            }
+
+            override fun onTimelineChanged(timeline: Timeline, manifest: Any?, reason: Int) {
+                // noop
+            }
+
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                // noop
+            }
+
+            override fun onSeekProcessed() {
+                // noop
+            }
+
+            override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
+                // noop
+            }
+
+        })
+
+
+    }
+    private fun pausePlayer() {
+        exoPlayer!!.setPlayWhenReady(false)
+        exoPlayer!!.getPlaybackState()
+    }
+
+    private fun startPlayer() {
+        exoPlayer!!.setPlayWhenReady(true)
+        exoPlayer!!.playbackState
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startPlayer()
+    }
+}
---
+++ app/src/main/java/com/live/tv/SplashActivity.kt
@@ -0,0 +1,268 @@
+package com.live.tv
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+
+
+class SplashActivity : AppCompatActivity() {
+
+    val PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+val preferences=getSharedPreferences("location", MODE_PRIVATE)
+        if (preferences.getBoolean("location",false)){
+            Handler().postDelayed({
+               checkLink()
+
+            }, 2000)
+
+        }
+        else
+        getLastLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        val gcd = Geocoder(applicationContext, Locale.getDefault())
+                        var Adress = gcd.getFromLocation(location.latitude, location.longitude, 1)
+                     //   val result = Adress[1].locality
+                        val add =Adress[0].featureName+","+Adress[0].subAdminArea+","+Adress[0].adminArea+","+Adress[0].countryCode
+                        Log.i("123321", "53:Result:${add}")
+                        addlocation(add)
+
+                    }
+                }
+            } else {
+               showEnableLocationSetting()
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var location: Location = locationResult.lastLocation
+            val gcd = Geocoder(applicationContext, Locale.getDefault())
+            var Adress = gcd.getFromLocation(location.latitude, location.longitude, 1)
+            //   val result = Adress[1].locality
+            val add =Adress[0].featureName+","+Adress[0].subAdminArea+","+Adress[0].adminArea+","+Adress[0].countryCode
+          addlocation(add)
+        }
+    }
+
+
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_ID
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
+    }
+
+
+    private fun addlocation(add: String) {
+
+
+        val ref=FirebaseDatabase.getInstance().getReference("location")
+        ref.push().setValue(add).addOnCompleteListener {
+            if (it.isSuccessful){
+                val editor=getSharedPreferences("location", MODE_PRIVATE).edit()
+                editor.putBoolean("location",true)
+                editor.apply()
+                        checkLink()
+                        
+            }
+        }
+        
+
+    }
+
+    private fun checkLink() {
+
+
+        val ref=FirebaseDatabase.getInstance().getReference("link")
+        ref.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java).putExtra("link",snapshot.value.toString()))
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun showEnableLocationSetting() {
+
+            val locationRequest = LocationRequest.create()
+            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+            val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+
+            val task = LocationServices.getSettingsClient(this)
+                .checkLocationSettings(builder.build())
+
+            task.addOnSuccessListener { response ->
+                val states = response.locationSettingsStates
+                if (states.isLocationPresent) {
+                    //Do something
+                }
+            }
+            task.addOnFailureListener { e ->
+                if (e is ResolvableApiException) {
+                    try {
+                        // Handle result in onActivityResult()
+                        e.startResolutionForResult(this, 999)
+                    } catch (sendEx: IntentSender.SendIntentException) { }
+                }
+            }
+        }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("onActivityResult()", Integer.toString(resultCode))
+        when (requestCode) {
+            999 -> when (resultCode) {
+                RESULT_OK -> {
+
+                    // All required changes were successfully made
+                    if (ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return
+                    }
+                    mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                        val location: Location? = task.result
+                        if (location == null) {
+                            requestNewLocationData()
+                        } else {
+                            val gcd = Geocoder(applicationContext, Locale.getDefault())
+                            var Adress =
+                                gcd.getFromLocation(location.latitude, location.longitude, 1)
+                            //   val result = Adress[1].locality
+                            val add =
+                                Adress[0].featureName + "," + Adress[0].subAdminArea + "," + Adress[0].adminArea + "," + Adress[0].countryCode
+                            Log.i("123321", "53:Result:${add}")
+                            addlocation(add)
+
+                        }
+                    }
+                }
+                RESULT_CANCELED -> {
+
+                    // The user was asked to change settings, but chose not to
+                    Toast.makeText(
+                        this,
+                        "Location not enabled, user cancelled.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                else -> {
+                }
+            }
+        }
+    }
+    }
